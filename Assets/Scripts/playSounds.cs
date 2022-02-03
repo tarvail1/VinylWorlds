8bd554ebbb6c2351deb05cc2100f0d8d6e275c21
tarvail1 <85487596+tarvail1@users.noreply.github.com>
2022-02-03 16:25:53 +0200

    added bike animations

--- Assets/Scripts/playSounds.cs
+++ Assets/Scripts/playSounds.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class playSounds : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-       AudioManager.AudioManagerInstance.Play("forest");
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void playHood()
+    {
+        AudioManager.AudioManagerInstance.Play("carHood");
     }
 }
