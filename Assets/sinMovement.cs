549bbc12a7f3a54c403e2aa10adfdae21ffb9b1e
tarvail1 <85487596+tarvail1@users.noreply.github.com>
2022-02-11 16:33:44 +0200

    Sinmovement and eagle added

---
+++ Assets/sinMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sinMovement : MonoBehaviour
+{
+    public float timePeriod = 2;
+    public float height = 30f;
+    private float timeSinceStart;
+    private Vector3 pivot;
+    private sinMovement _sinMovement; 
+
+    private void Start()
+    {
+        pivot = transform.position;
+        height /= 2;
+        timeSinceStart = (3 * timePeriod) / 4;
+        
+    }
+
+    void Update()
+    {
+        Vector3 nextPos = transform.position;
+        nextPos.y = pivot.y + height + height * Mathf.Sin(((Mathf.PI * 2) / timePeriod) * timeSinceStart);
+        timeSinceStart += Time.deltaTime;
+        transform.position = nextPos;
+    }
+
+}
